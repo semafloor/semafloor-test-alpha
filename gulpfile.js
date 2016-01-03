685d7734e24f3f0b2ae5e0e38f4a62cc26ece21f
motss <wes_ngrongsen@hotmail.com>
2016-01-04 02:33:02 +0800

    :honeybee: added and tested optimized reserve-page

--- gulpfile.js
+++ gulpfile.js
@@ -136,6 +136,7 @@ gulp.task('images', function() {
 gulp.task('copy', function() {
   var app = gulp.src([
     'app/*',
+    'app/**/*.json', // workaround to copy json/
     '!app/test',
     '!app/cache-config.json'
   ], {
