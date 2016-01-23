c0f3e984e745cc574a3d3cb95ba96fe119b76e8e
motss <wes_ngrongsen@hotmail.com>
2016-01-23 23:18:36 +0800

    :sailboat: removed importAnotherHref after each page attached, removed hashbang for server URL rewrites, tidy up all async functions

--- app/elements/semafloor-test/semafloor-test.js
+++ app/elements/semafloor-test/semafloor-test.js
@@ -67,12 +67,12 @@
       'reserve-page-attached': '_onReservePageAttached',
       'search-page-attached': '_onSearchPageAttached',
       'current-page-attached': '_onCurrentPageAttached',
-      'room-page-attached': '_onRoomPageAttached',
-      'profile-upgraded': 'importAnotherHref',
-      'reserve-upgraded': 'importAnotherHref',
-      'search-upgraded': 'importAnotherHref',
-      'current-upgraded': 'importAnotherHref',
-      'room-upgraded': 'importAnotherHref'
+      'room-page-attached': '_onRoomPageAttached'
+      // 'profile-upgraded': 'importAnotherHref',
+      // 'reserve-upgraded': 'importAnotherHref',
+      // 'search-upgraded': 'importAnotherHref',
+      // 'current-upgraded': 'importAnotherHref',
+      // 'room-upgraded': 'importAnotherHref'
     },
 
     // 'created': function() {
@@ -108,85 +108,92 @@
 
     _notifyResizeAppHeaderOnCategoryChange: function() {
       var _category = this.category;
-      console.log('app-header-notify-resize', this.category);
+      // console.log('1) app-header-notify-resize', this.category);
       // Dynamically HTML importing pages.
       if (_category !== 'home' && !this['_' + _category + 'Attached']) {
         var _dynamicallyImportHrefs = '../../bower_components/semafloor-' + _category +
           '-page/semafloor-' + _category + '-page.html';
         this.importHref(_dynamicallyImportHrefs, function() {
-          console.log(_category + ' is loaded.');
+          // console.log('5) ' + _category + ' is loaded.');
         }, function(error) {
           console.error(_category, error);
         });
       }
       // Always notifyResize async-ly after 1.
-      this.async(function() {
-        this.$.mainHeader.notifyResize();
-        // workaround for missing loaded list.
-        // when reserve page is attached and list has been loaded but does not show.
-        if (_category === 'reserve' && this._reserveAttached) {
-          this.$.fakeTabs.notifyResize();
-          this.$.reserve.updateReservePages();
-        }else if (_category === 'current' && this._currentAttached) {
-          this.$.current.updateCurrentPages();
-        }else if (_category === 'room' && this._roomAttached) {
-          this.$.room.updateIronImageWidth();
-        }
-        // after 300ms.
-      }, 300);
+      // this.async(function() {
+      // console.log('2) ' + _category + ' after loaded!');
+      // }, 1);
     },
 
     // page attached event.
     _onProfilePageAttached: function() {
+      // console.log('3) on-profile-page-attached');
       this.async(function() {
+        // console.log('4) profile-page-notify-resize');
         this.set('_profileAttached', true);
-        this.fire('profile-upgraded', 'reserve');
+        this.$.mainHeader.notifyResize();
+        // this.fire('profile-upgraded', 'reserve');
       }, 1);
     },
     _onReservePageAttached: function() {
       // Always notifyResize async-ly after 1.
+      // console.log('3) on-reserve-page-attached');
       this.async(function() {
-        // console.log('reserve-page-notify-resize');
+        // console.log('4) reserve-page-notify-resize');
         this.set('_reserveAttached', true);
         this.$.mainHeader.notifyResize();
-        this.fire('reserve-upgraded', 'search');
+        this.$.fakeTabs.notifyResize();
+        this.$.reserve.updateReservePages();
+        // this.fire('reserve-upgraded', 'search');
       }, 1);
     },
     _onSearchPageAttached: function() {
+      // console.log('3) on-search-page-attached');
       this.set('_searchAttached', true);
-      this.fire('search-upgraded', 'current');
+      this.async(function() {
+        // console.log('4) search-page-notify-resize');
+        this.$.mainHeader.notifyResize();
+      }, 1);
+      // this.fire('search-upgraded', 'current');
     },
     _onCurrentPageAttached: function() {
+      // console.log('3) on-current-page-attached');
       this.set('_currentAttached', true);
-      this.fire('current-upgraded', 'room');
+      this.async(function() {
+        // console.log('4) current-page-notify-resize');
+        this.$.mainHeader.notifyResize();
+        this.$.current.updateCurrentPages();
+      }, 1);
+      // this.fire('current-upgraded', 'room');
     },
     _onRoomPageAttached: function() {
+      // console.log('3) on-room-page-attached');
       this.async(function() {
-        this.$.room.updateIronImageWidth();
+        // console.log('4) room-page-notify-resize');
+        this.$.mainHeader.notifyResize();
+        this.$.room.updateIronImageWidth(this.getBoundingClientRect().width);
         // after 10ms.
-        this.fire('room-upgraded', 'profile');
-      }, 10);
+        // this.fire('room-upgraded', 'profile');
+      }, 1);
       this.set('_roomAttached', true);
     },
 
     // dynamically import subsequent href.
-    importAnotherHref: function(ev) {
-      // when it's room page OR when the requested page has been ready no import is needed.
-      if (ev.detail[0] === undefined || this['_' + ev.detail + 'Attached']) {
-        return;
-      }
-
-      var _category = ev.detail;
-      var _dynamicallyImportHrefs = '../../bower_components/semafloor-' + _category +
-        '-page/semafloor-' + _category + '-page.html';
-      this.async(function() {
-        this.importHref(_dynamicallyImportHrefs, function() {
-          console.log(_category + ' is loaded.');
-        }, function(error) {
-          console.error(_category, error);
-        });
-      }, 1);
-    },
+    // importAnotherHref: function(ev) {
+    //   // when it's room page OR when the requested page has been ready no import is needed.
+    //   if (ev.detail[0] === undefined || this['_' + ev.detail + 'Attached']) {
+    //     return;
+    //   }
+    //
+    //   var _category = ev.detail;
+    //   var _dynamicallyImportHrefs = '../../bower_components/semafloor-' + _category +
+    //     '-page/semafloor-' + _category + '-page.html';
+    //   this.importHref(_dynamicallyImportHrefs, function() {
+    //     console.log(_category + ' is loaded.');
+    //   }, function(error) {
+    //     console.error(_category, error);
+    //   });
+    // },
 
   });
 })();
